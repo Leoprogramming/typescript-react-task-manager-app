4729d3512c9ce0dcb685abfb792ce478646ab88c
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-19 10:59:11 +0200

    built toggleTodo control flow

--- task-manager-react-typescript/src/App.tsx
+++ task-manager-react-typescript/src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoListItem } from './TodoListItem';
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "Study", complete: true }, 
   { text: "Code", complete: false }
 ];
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo;
+    })
+    setTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
       <TodoListItem todo={todos[0]} />
